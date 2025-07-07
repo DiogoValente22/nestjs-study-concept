c65a20d13a87dbb4094d5356186ecc7c9d5b240f
DiogoValente22 <diogovalentearagao@outlook.com>
2025-07-06 22:51:15 -0300

    feat: install bullmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MathModule } from './math/math.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -12,7 +11,6 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
-    MathModule,
     UserModule,
   ],
   controllers: [AppController],
